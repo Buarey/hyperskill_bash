7806a8c3e0743bbfdc928f0e7b4642e64f66863c
Buarey <render@borg-tech.pl>
2023-12-09 12:39:49 +0100

    Simple Converter Stage 5

--- simple_converter.bash
+++ simple_converter.bash
@@ -7,22 +7,18 @@ file_name="definitions.txt"
 converter () {
 
 if [ $1 == "1" ]; then
-
-#	def_valid
-		echo "Not implemented!"
-		menu
-		read input	
-
+	def_check "math" "convert units"
+	menu
+	read input
 elif [ $1 == "2" ]; then 
 	def_valid
 	menu
 	read input
 elif [ $1 == "3" ]; then
-	del_def
+	def_check "delete" "delete"
 	menu
 	read input	
 fi
-
 }
 
 def_valid () {
@@ -59,55 +55,72 @@ add_def () {
 	echo "$full_input" >> "$file_name"
 }
 
-del_def () {
-	file_weight=$(wc -c $file_name | cut -d " " -f 1)
-	line_number=$(wc -l $file_name | cut -d " " -f 1)
-	
-	
-		if [[ $file_weight == "0" ]]; then
+delete () {
+	read del_line
+		
+	while [[ $del_line != "0" ]]
+	do
+		
+		if [[ $del_line -gt $line_number ]] || [[ -z $del_line ]]; then
+			echo "Enter a valid line number!"
+			read del_line
+		else
+			sed -i "${del_line}d" "$file_name"
+			break
+		fi	
+	done
+}
+
+def_check () {
+
+operation=$1
+target=$2
+
+file_weight=$(wc -c $file_name | cut -d " " -f 1)
+line_number=$(wc -l $file_name | cut -d " " -f 1)
+
+	if [[ $file_weight == "0" ]]; then
 			echo "Please add a definition first!"
 		else
 		
-		echo "Type the line number to delete or '0' to return"
+		echo "Type the line number to $target or '0' to return"
 
+			#LIST CONTENTS OF FILE	
+			nl -w1 -s'. ' $file_name
 
-		nl -w1 -s'. ' $file_name
-
-		read del_line
-		
-			while [[ $del_line != "0" ]]
-			do
-		
-			if [[ $del_line -gt $line_number ]] || [[ -z $del_line ]]; then
-				echo "Enter a valid line number!"
-				read del_line
-			else
-				sed -i "${del_line}d" "$file_name"
-				break
-			fi	
-			done
-		fi	
+			$operation
+		fi
 }
 
 math () {
+re2='^[-+]?[0-9]+\.?[0-9]*$'
 
-	echo "Enter a value to convert:"
-		read conv_value
-		re2='^[-+]?[0-9]+\.?[0-9]*$'	
-
-		while [[ ! "$conv_value" =~ $re2 ]]	
-			do
-			if [[ ! "$conv_value" =~ $re2 ]]; then
-				echo "Enter a float or integer value!"
-				read conv_value	
-			fi
-		done
-	
-		result=$(bc <<< "$float * $conv_value")
-		echo "Result: $result"
+read line
 		
-		menu
-		read input
+while [[ $line != "0" ]]
+	do
+		
+		if [[ $line -gt $line_number ]] || [[ -z $line ]] || [[ ! $line =~ $re2 ]]; then
+			echo "Enter a valid line number!"
+			read line
+		else
+			to_conv=$(sed "${line}!d" "$file_name" | cut -d " " -f 2) 
+			
+			echo "Enter a value to convert:"
+			read conv_value
+			
+			while [[ ! "$conv_value" =~ $re2 ]]	
+				do
+					if [[ ! "$conv_value" =~ $re2 ]]; then
+						echo "Enter a float or integer value!"
+					read conv_value	
+					fi
+				done
+			result=$(bc <<< "$to_conv * $conv_value")
+			echo "Result: $result"
+			break
+		fi	
+done
 }
 
 menu () {
@@ -117,7 +130,6 @@ menu () {
 	echo "1. Convert units"
 	echo "2. Add a definition"
 	echo "3. Delete a definition"
-
 }
 
 #main
