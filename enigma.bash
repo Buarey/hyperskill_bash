4536a8053021e78911082846f41da543ec369b1e
Buarey <render@borg-tech.pl>
2023-12-10 18:01:23 +0100

    Stage 2 Enigma

--- enigma.bash
+++ enigma.bash
@@ -1,15 +1,55 @@
 #!/bin/bash
+#global variables
+re='^[A-Z ]+$'
+re2='^[0-9]$'
+re3='^[A-Z]$'
 
-
-#main
-
+#functions
+valid_msg () {
 echo "Enter a message:"
 read -r input
 
-re='^[A-Z ]+$'
 
 if [[ "$input" =~ $re ]]; then
     echo "This is a valid message!"
 else
     echo "This is not a valid message!"
 fi
+
+}
+
+ascii_conv () {
+
+echo "Enter an uppercase letter:"
+read -r letter
+echo "Enter a key:"
+read -r key
+
+
+if [[ "$letter" =~ $re3 ]] && [[ $key =~ $re2 ]]; then
+    
+	
+	value=$(printf "%d\n" "'$letter")
+	encoded_letter=$(( $value + $key ))
+	
+	if [[ $encoded_letter -gt 90 ]]; then 
+		reverse=$(( $encoded_letter - 26 ))
+		char=$(printf "%b\n" "$(printf "\\%03o" "$reverse")")
+	else	
+		char=$(printf "%b\n" "$(printf "\\%03o" "$encoded_letter")")
+	fi
+	
+	echo "Encrypted letter: $char"
+else
+    echo "Invalid key or letter!"
+fi
+
+
+
+}
+
+#main
+
+#valid_msg
+
+ascii_conv
