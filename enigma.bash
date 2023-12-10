d4459165aa519adb7c80be9980b27a9da2093209
Buarey <render@borg-tech.pl>
2023-12-10 17:24:10 +0100

    Stage 1 Enigma

---
+++ enigma.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+#main
+
+echo "Enter a message:"
+read -r input
+
+re='^[A-Z ]+$'
+
+if [[ "$input" =~ $re ]]; then
+    echo "This is a valid message!"
+else
+    echo "This is not a valid message!"
+fi
