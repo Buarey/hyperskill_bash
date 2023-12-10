7051205e358ce3c2b54c2511c7cae7629a6c8096
Buarey <render@borg-tech.pl>
2023-12-10 12:44:01 +0100

    Simple Calculator Stage 1

---
+++ simple_calculator.bash
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+#global variables
+re='^[0-9]+$'
+re2='^[-+]?[0-9]+\.?[0-9]*$'
+calc_type=""
+first_int=""
+scnd_int=""
+
+#################
+
+#functions
+
+read_int () {
+	echo "Enter an arithmetic operation:"
+	read $first_int $calc_type $scnd_int
+
+}
+
+integer_check () {
+if [[ ! $first_int =~ $re ]] || [[ ! $scnd_int =~ re ]] || [[ -z $first_int ]] || [[ -z $scnd_int ]]; then 
+	echo "Operation check failed!"
+else 
+	echo "Operation check passed!"
+
+fi
+	
+}
+
+##########
+
+#main
+
+read_int
+integer_check
+
+
