e165c68222c8941f011ab29179cb2c5ead2e2c25
Buarey <render@borg-tech.pl>
2023-12-10 15:05:27 +0100

    Simple Calculator Stage 4

--- simple_calculator.bash
+++ simple_calculator.bash
@@ -12,30 +12,40 @@ scnd_int=""
 #functions
 
 read_int () {
-	echo "Enter an arithmetic operation:"
-	read first_int calc_type scnd_int
-
+	echo "Enter an arithmetic operation or type 'quit' to quit:" | tee -a operation_history.txt
+	read first_int calc_type scnd_int 
+	echo $first_int $calc_type $scnd_int >> operation_history.txt
 
 }
 
 integer_check () {
 if [[ ! $first_int =~ $re ]] || [[ ! $scnd_int =~ $re ]] || [[ -z $first_int ]] || [[ -z $scnd_int ]]; then 
-	echo "Operation check failed!"
+	echo "Operation check failed!" | tee -a operation_history.txt
 else 
 #	echo "Operation check passed!"
-	math
+	math 
 
 fi
 }
 
 math () {
 	result=$(bc <<< "$first_int $calc_type $scnd_int")
-	echo $result
+	echo $result | tee -a operation_history.txt
 }
 
 ##########
 
 #main
+touch operation_history.txt
+
+echo "Welcome to the basic calculator!" | tee -a operation_history.txt
 
 read_int
-integer_check
+
+while [[ $first_int != "quit" ]] 
+do 
+	integer_check
+	read_int
+done
+
+echo "Goodbye!" | tee -a operation_history.txt
