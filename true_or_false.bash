b1301cf0f4832c1c2d556303b7e7c46354dc276a
Buarey <render@borg-tech.pl>
2023-12-19 20:38:23 +0100

    True or Flase Stage 2

--- true_or_false.bash
+++ true_or_false.bash
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+stage_one () {
+
+	curl http://127.0.0.1:8000/download/file.txt --silent --output <ID_card.txt>
+	cat ID_card.txt
+}
+
 echo "Welcome to the True or False Game!"
 
-curl http://127.0.0.1:8000/download/file.txt --silent --output <ID_card.txt>
-cat ID_card.txt
+curl http://127.0.0.1:8000/download/login --silent --cookie-jar cookie.txt --user <username:password>
+echo "Login message: the-message-from-endpoint"
